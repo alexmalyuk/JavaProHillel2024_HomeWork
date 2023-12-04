b05035e887b942ad2955cf20b70e22e4e994c2f5
Olexandr Malyuk <alex.malyuk@gmail.com>
2023-12-04 23:31:16 +0200

    Delete src/sandbox directory

--- src/sandbox/Test.java
+++
@@ -1,9 +0,0 @@
-package sandbox;
-
-public class Test {
-    public static void main(String[] args) {
-        String s1 = "Aaaa";
-        String s2 = "Bbbb";
-        System.out.println(s1.compareTo(s2));
-    }
-}
